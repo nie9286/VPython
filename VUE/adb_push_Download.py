da4ca5dbba850157b93e42b0b8efeb5c78105f83
Visin1991 <zhuzhanhao1991@qq.com>
2020-08-03 17:56:56 +0800

    add adb tools

---
+++ VUE/adb_push_Download.py
@@ -0,0 +1,29 @@
+import os
+import re
+
+filename = "push_test.txt"
+device_dir = '/sdcard/Download'
+device_file = device_dir + "/"+ filename
+
+
+def get_devices():
+    p = os.popen('adb devices')
+    devices_str = p.read()
+    devices = re.findall(r'(\w+)\s+device\s', devices_str)
+    return devices
+
+def pushFile(device,target_file,device_file):
+    cmd = 'adb -s %s push %s %s' % (device,target_file,device_file)
+    print(cmd)
+    p = os.popen(cmd)
+    result = p.read()
+    print (result)
+
+devices = get_devices()
+for device in devices:
+    print(device)
+    pushFile(device,filename,device_file)
+
+
+
+
---
+++ VUE/adb_ue_stat_unit.py
@@ -0,0 +1,25 @@
+import os
+import re
+
+def get_devices():
+    p = os.popen('adb devices')
+    devices_str = p.read()
+    devices = re.findall(r'(\w+)\s+device\s', devices_str)
+    return devices
+
+def shellcmd(device_id,command):
+    cmd = 'adb -s %s shell "%s"' % (device_id, command)
+    print(cmd)
+    p = os.popen(cmd)
+    result = p.read()
+    print(result)
+
+devices = get_devices()
+for device in devices:
+    print(device)
+    shellcmd(device,"am broadcast -a android.intent.action.RUN -e cmd 'stat unit'")
+    shellcmd(device,"am broadcast -a android.intent.action.RUN -e cmd 'stat fps'")
+
+
+
+
